8a66272e256919f451ee12e1d16d371e0750ab13
root <root@cnbjlx25554.corpusers.net>
2018-12-28 17:16:01 +0800

    add linear regression

--- perceptron.py
+++ perceptron.py
@@ -1,28 +1,43 @@
 import numpy as np
+from functools import reduce
 
 class Perceptron:
+    def __init__(self, input_num, activator):
 
-    def __init__(self, input_length, weights = None):
-        if weights is None:
-            self.weights = np.ones(input_length) * 0.5
-        else:
-            self.weights = weights
-
-    @staticmethod
-    def unit_step_function(x):
-        if x > 0.5:
-            return 1
-        return 0
-
-    def __call__(self, in_data):
-        weighted_input = self.weights * in_data
-        weighted_sum = weighted_input.sum()
-        return Perceptron.unit_step_function(weighted_sum)
+        self.activator = activator
+        self.weights = [0.0 for _ in range(input_num)]
+        self.bias = 0.0
 
 
-p = Perceptron(2, np.array([0.5, 0.5]))
-for x in [np.array([0, 0]), np.array([0, 1]),
-          np.array([1, 0]), np.array([1, 1])]:
-    y = p(np.array(x))
-    print(x, y)
-            
+    def __str__(self):
+
+        return 'weights\t:%s\nbias\t:%f\n' % (self.weights, self.bias)
+
+
+    def predict(self, input_vec):
+
+        return self.activator(np.dot(input_vec, list(self.weights))+ self.bias)
+    
+
+    def train(self, input_vecs, labels, iteration, rate):
+
+        for i in range(iteration):
+            self._one_iteration(input_vecs, labels, rate)
+
+    def _one_iteration(self, input_vecs, labels, rate):
+
+        samples = zip(input_vecs, labels)
+        for (input_vec, label) in samples:
+
+            output = self.predict(input_vec)
+            self._update_weights(input_vec, output, label, rate)
+
+    def _update_weights(self, input_vec, output, label, rate):
+
+        delta = label - output
+        self.weights = [w + rate * delta * x 
+            for x, w in zip(input_vec, self.weights)]
+        self.bias += rate * delta
+
+        
+        
