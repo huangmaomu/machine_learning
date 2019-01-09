c84a7b20830c63652c9d517902990702c984f546
root <root@cnbjlx25554.corpusers.net>
2019-01-09 17:53:18 +0800

    add parameters tuning

--- fc.py
+++ fc.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.utils import np_utils
+from keras import optimizers
 
 seed = 7
 numpy.random.seed(seed)
@@ -27,10 +28,15 @@ num_classes = y_test.shape[1]
 
 def baseline_model():
     model = Sequential()
+    #model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation = 'sigmoid'))
     model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation = 'relu'))
+    #model.add(Dense(num_pixels, kernel_initializer='normal', activation = 'relu'))
     model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
     #compile model
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    #model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer=optimizers.SGD(lr=0.1), metrics=['accuracy'])
+
     return model 
 
 
@@ -39,6 +45,7 @@ if __name__ == "__main__":
     model = baseline_model()
     #Fit the model
     model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=200, verbose=2)
+    #model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=10, batch_size=100, verbose=2)
     #Final evalution of the model
     scores = model.evaluate(x_test, y_test, verbose=0)
     print("Error: %.2f%%" %(100-scores[1]*100))
